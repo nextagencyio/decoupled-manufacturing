302339cca717822a9ed67f45ec676deaf32c726d
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/facilities/page.tsx
+++ app/facilities/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import FacilityCard from '../components/FacilityCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Facilities | Manufacturing',
